7cdb39b6a34c84256c8fcbc1542149a0f60c2115
Parvez Mosharaf Siam <86480227+pmsiam0@users.noreply.github.com>
2025-08-15 18:38:13 +0600

    tools.sh

--- tools.sh
+++ tools.sh
@@ -30,7 +30,7 @@ chmod 755 /usr/local/bin/findomain
 go install github.com/lc/gau/v2/cmd/gau@latest
 
 go install github.com/tomnomnom/waybackurls@latest
-
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install github.com/gwen001/github-subdomains@latest
 
 go install github.com/tomnomnom/anew@latest
